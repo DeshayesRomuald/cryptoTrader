14323f32ab9b218a1e815a6a14168c7c8334616e
romuald <romuald@qover.me>
2017-09-07 15:12:23 +0200

    try to make sounf not there yet ...

--- utils/notifier.js
+++ utils/notifier.js
@@ -1,10 +1,19 @@
 const notifier = require('node-notifier');
 
+var Sound = require('node-aplay');
+
+// fire and forget:
+const sound = new Sound('../media/sound/3721.mp3');
+
+
+
 const USE_NOTIFICATIONS = true;
 
 function notify(title = 'Should BUY', message = `Value is ${buyValue}`) {
   //notify user with system notification to buy
   if (USE_NOTIFICATIONS) {
+    sound.play();
+
     notifier.notify({
       title,
       message,
