4789835a09a98ed09300aad61c21591dee637a5a
romuald <romuald@qover.me>
2017-09-12 16:31:39 +0200

    attach wallet to watcher

--- factories/cryptoWatcherFactory.js
+++ factories/cryptoWatcherFactory.js
@@ -1,11 +1,11 @@
 const CryptoWatcher = require('../models/CryptoWatcher');
 const slidingWindowFactory = require('../factories/slidingWindowFactory');
 
-function create(name) {
+function create(name, wallet) {
   if (!name) {
     return null;
   }
-  const cryptoWatcher = new CryptoWatcher();
+  const cryptoWatcher = new CryptoWatcher(wallet);
   cryptoWatcher.slidingWindow = slidingWindowFactory.create(name);
 
   cryptoWatcher.slidingWindow.authorizedDiffPosNeg = cryptoWatcher.authorizedDiffPosNeg;
